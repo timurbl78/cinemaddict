7e9cefb232f917287929b4a56d7a963ce1d58e17
Тимур Белов <timurbl574@gmail.com>
2021-04-20 18:29:54 +0300

    activate showMoreButton

--- src/main.js
+++ src/main.js
@@ -12,7 +12,9 @@ import {generateFilter} from './mock/filter';
 import { createFilmPopupTemplate } from './view/film-popup';
 import {createFooterStatisticsTemplate} from './view/footer-statistics';
 
-const FILMS_COUNT = 15;
+
+const FILMS_COUNT = 20;
+const FILMS_COUNT_PER_STEP = 5;
 const FILMS_EXTRA_COUNT = 2;
 
 const data = new Array(FILMS_COUNT + FILMS_EXTRA_COUNT * 2).fill(null).map(() => ({
@@ -38,12 +40,29 @@ const siteFilmsContainerElement = siteMainElement.querySelector('.films');
 render(siteFilmsContainerElement, createFilmsListTemplate(), 'beforeend');
 const siteFilmsListContainerElement = siteFilmsContainerElement.querySelector('.films-list__container');
 
-for (let i = 0; i < FILMS_COUNT; i++) {
-  render(siteFilmsListContainerElement, createFilmCardTemplate(data[i]), 'beforeend');
+if (data.length > FILMS_COUNT_PER_STEP) {
+  let renderedFilmCount = FILMS_COUNT_PER_STEP;
+  render(siteFilmsListContainerElement, createShowMoreButtonTemplate(), 'afterend');
+
+  const showMoreButton = siteMainElement.querySelector('.films-list__show-more');
+  showMoreButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    data
+      .slice(renderedFilmCount, renderedFilmCount + FILMS_COUNT_PER_STEP)
+      .forEach((el) => render(siteFilmsListContainerElement, createFilmCardTemplate(el), 'beforeend'))
+
+    renderedFilmCount += FILMS_COUNT_PER_STEP;
+
+    if (renderedFilmCount >= data.length) {
+      showMoreButton.remove();
+    }
+  })
 }
 
-render(siteFilmsListContainerElement, createShowMoreButtonTemplate(), 'afterend');
-
+for (let i = 0; i < Math.min(data.length, FILMS_COUNT_PER_STEP); i++) {
+  render(siteFilmsListContainerElement, createFilmCardTemplate(data[i]), 'beforeend');
+}
 
 render(siteFilmsContainerElement, createFilmsListExtraTemplate('Top rated'), 'beforeend');
 render(siteFilmsContainerElement, createFilmsListExtraTemplate('Most commented'), 'beforeend');
